ad86275d4197a37848f9b96c4cc6fd19ee9f0e63
Fran <89055928+Fran26TE@users.noreply.github.com>
2021-09-01 18:47:36 +0200

    programación de la rotación de las agujas

---
+++ script.js
@@ -0,0 +1,31 @@
+function reloj() {
+	time = new Date();
+	horas = time.getHours();
+	minutos = time.getMinutes();
+	segundos = time.getSeconds();
+    
+    if (horas >= 12) {
+        // 12 posiciones de aguja, 360 grados de giro
+     	porcentajeHoras = horas / 12 * 360;
+    } else {
+        porcentajeHoras = horas / 24 * 360;
+    }
+
+    // 30 son los grados entre cada hora
+    porcentajeHoras += minutos / 60 * 30;
+
+    // la rotación de los minutos es de 360 grados
+    porcentajeMinutos = minutos / 60 * 360;
+
+    // la rotación de los segundos es de 360 grados
+    porcentajeSegundos = segundos / 60 * 360;
+
+    // style.transform = "rotate(""deg)"; para la rotación de las agujas.
+    document.getElementById("horas").style.transform = "rotate("+ porcentajeHoras +"deg)";
+    document.getElementById("minutos").style.transform = "rotate("+ porcentajeMinutos +"deg)";
+    document.getElementById("segundos").style.transform = "rotate("+ porcentajeSegundos +"deg)";
+ 
+}
+
+// Llama a la función reloj cada 1 segundo
+setInterval(reloj, 1000);
