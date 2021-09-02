e8e25f797c3956992fafae0a4b2bc4e03b16f5cf
Fran <89055928+Fran26TE@users.noreply.github.com>
2021-09-02 05:37:47 +0200

    arreglo angulos de las agujas

--- script.js
+++ script.js
@@ -1,33 +1,34 @@
 function reloj() {
-	time = new Date();
-	horas = time.getHours();
-	minutos = time.getMinutes();
-	segundos = time.getSeconds();
-    
-    if (horas >= 12) {
-        // 12 posiciones de aguja, 360 grados de giro
-     	porcentajeHoras = horas / 12 * 360;
-    } else {
-        porcentajeHoras = horas / 24 * 360;
-    }
+    time = new Date();
+    horas = time.getHours();
+    minutos = time.getMinutes();
+    segundos = time.getSeconds();
 
-    // 30 son los grados entre cada hora
-    porcentajeHoras += minutos / 60 * 30;
 
-    // la rotación de los minutos es de 360 grados
-    porcentajeMinutos = minutos / 60 * 360;
-
-    // la rotación de los segundos es de 360 grados
-    porcentajeSegundos = segundos / 60 * 360;
+    // Ángulos de rotación de las agujas
+    anguloHoras = 360 * (horas % 12) / 12;
+    anguloMinutos = 360 * minutos / 60
+    anguloSegundos = 360 * segundos / 60
 
     // style.transform = "rotate(""deg)"; para la rotación de las agujas.
-    document.getElementById("horas").style.transform = "rotate("+ porcentajeHoras +"deg)";
-    document.getElementById("minutos").style.transform = "rotate("+ porcentajeMinutos +"deg)";
-    document.getElementById("segundos").style.transform = "rotate("+ porcentajeSegundos +"deg)";
+    document.getElementById("horas").style.transform = "rotate(" + anguloHoras + "deg)";
+    document.getElementById("minutos").style.transform = "rotate(" + anguloMinutos + "deg)";
+    document.getElementById("segundos").style.transform = "rotate(" + anguloSegundos + "deg)";
+
+    // Evitar números individuales
+    if(horas<10){
+        horas = "0" + horas;
+    }
+    if(minutos<10){
+        minutos = "0" + minutos;
+    }
+    if(segundos<10){
+        segundos = "0" + segundos;
+    }
 
     // Añade al parrafo el resultado de las horas, minutos y segundos
-    document.getElementById("horaDigital").innerHTML = horas + ":" + minutos + ":" + segundos; 
- 
+    document.getElementById("horaDigital").innerHTML = horas + ":" + minutos + ":" + segundos;
+
 }
 
 // Llama a la función reloj cada 1 segundo
